d53e9fe5a8ae9459f483821d2f748fefca508f2f
Will Cygan <wcygan.io@gmail.com>
2022-05-06 22:10:58 -0500

    added for loop & added to .gitignore

--- app/src/main/java/io/wcygan/App.java
+++ app/src/main/java/io/wcygan/App.java
@@ -9,6 +9,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(new App().getGreeting());
+        }
     }
 }
