b9203a6dace92376e85ba67fb873060bb34a9f8f
Will Cygan <wcygan.io@gmail.com>
2025-04-29 18:24:46 -0500

    server runs

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"buf.build/gen/go/wcygan/hello/connectrpc/go/hello/v1/hellov1connect"
+	v1 "buf.build/gen/go/wcygan/hello/protocolbuffers/go/hello/v1"
+	"connectrpc.com/connect"
+	"connectrpc.com/grpcreflect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type GreeterServer struct{}
+
+func (s *GreeterServer) SayHello(
+	ctx context.Context,
+	req *connect.Request[v1.SayHelloRequest],
+) (*connect.Response[v1.SayHelloResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&v1.SayHelloResponse{
+		Message: fmt.Sprintf("Hello %s!", req.Msg.Name),
+	})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+	greeter := &GreeterServer{}
+	mux := http.NewServeMux()
+	path, handler := hellov1connect.NewGreeterServiceHandler(greeter)
+	mux.Handle(path, handler)
+
+	reflector := grpcreflect.NewStaticReflector(
+		hellov1connect.GreeterServiceName, // Add the service name here
+	)
+	mux.Handle(grpcreflect.NewHandlerV1(reflector))
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(reflector))
+
+	fmt.Println("Server listening on :8080 with reflection enabled")
+	err := http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+		// mux, // Simple HTTP/1.1 for now
+	)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- server/go.mod
+++ server/go.mod
@@ -5,4 +5,9 @@ go 1.24.2
 require (
 	buf.build/gen/go/wcygan/hello/connectrpc/go v1.18.1-20250429225917-23d5973e7dd6.1 // indirect
 	buf.build/gen/go/wcygan/hello/protocolbuffers/go v1.36.6-20250429225917-23d5973e7dd6.1 // indirect
+	connectrpc.com/connect v1.18.1 // indirect
+	connectrpc.com/grpcreflect v1.3.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
